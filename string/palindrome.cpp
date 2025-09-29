c4c86c58ae6b0dedc3e8d249e81a7bce6e7f22ca
Harun mansuri <mrharun961766@gmail.com>
2025-09-29 09:53:40 +0530

    string practice

--- string/palindrome.cpp
+++ string/palindrome.cpp
@@ -1,34 +1,56 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
+char tolowercase(char ch) {
+    if (ch >= 'a' && ch <= 'z') {
+        return ch;
+    } else if (ch >= 'A' && ch <= 'Z') {
+        return ch - 'A' + 'a';
+    }
+    return ch;
+}
 
-bool palindrome(char s[],int n){
-    int start=0;
-    int end=n-1;
-    while(start<=end){
-        if(s[start]!=s[end]){
-            return false;
-        }
-        else{
+bool isValid(char ch) {
+    if ((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z') || (ch >= '0' && ch <= '9')) {
+        return true;
+    }
+    return false;
+}
+
+bool palindrome(string s) {
+    int start = 0;
+    int end = s.size() - 1;
+
+    while (start <= end) {
+        // agar character valid nahi hai to ignore karo
+        if (!isValid(s[start])) {
+            start++;
+        } 
+        else if (!isValid(s[end])) {
+            end--;
+        } 
+        else {
+            if (tolowercase(s[start]) != tolowercase(s[end])) {
+                return false;
+            }
             start++;
             end--;
         }
     }
     return true;
 }
-int main(){
-    char s[20];
-    cin>>s;
-    int n=strlen(s);
-    bool ans = palindrome(s,n);
-    if(ans){
-        cout<<"palindrome";
-    }
-    else{
-        cout<<"not a palindrome";
+
+int main() {
+    string s;
+    getline(cin, s);   // pura line input ke liye
+
+    bool ans = palindrome(s);
+
+    if (ans) {
+        cout << "palindrome";
+    } else {
+        cout << "not a palindrome";
     }
 
     return 0;
-
-    
 }
