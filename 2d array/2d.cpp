c1e43459967166d4e8c7cccda79f14e2dc7235ba
Harun mansuri <mrharun961766@gmail.com>
2025-10-03 09:49:39 +0530

    2d search

--- 2d array/2d.cpp
+++ 2d array/2d.cpp
@@ -2,28 +2,57 @@
 
 using namespace std;
 
+
+bool arrSearch(int arr[][3], int target,int n,int m){
+    for(int i=0; i<n; i++){
+        for(int j=0; j<m; j++){
+            if(arr[i][j]== target){
+                return 1;
+            }
+        }
+    }
+    return 0;
+
+}
 int main(){
-    int arr[3][4];
-   /* //takking row wise input
+
+    int arr[3][3]; //declaration
+
+    
+   // int arr[3][4] = {1,2,3,4,5,6,7,8,9,0,1,1};
+   //taking row wise input
     for(int row=0;row<3;row++){
-        for(int col=0;col<4;col++){
+        for(int col=0;col<3;col++){
             cin>>arr[row][col];
         }
-    }*/
+    }
 
-   //taking col wise input 
-    for(int col=0;col<4;col++){
+
+
+   //taking col wise input
+   /* for(int col=0;col<4;col++){
         for(int row=0;row<3;row++){
             cin>>arr[row][col];
         }
     }
+        */
 
     //print
     for(int row=0; row<3; row++){
-        for(int col=0; col<4; col++){
+        for(int col=0; col<3; col++){
             cout<<arr[row][col]<<" ";
         }
     }
+    cout<<endl;
+    cout<<"enter the element to be searched:-";
+    int key;
+    cin>>key;
 
+    if(arrSearch(arr,key,3,3)){
+        cout<<"element found"<<endl;
+    }
+    else{
+        cout<<"elemet not found" <<endl;
+    }
     return 0;
 }
