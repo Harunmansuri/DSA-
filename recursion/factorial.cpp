42359acc299b25e64fe93c7f05f8415cdb2e599e
Harun mansuri <mrharun961766@gmail.com>
2025-12-21 10:33:47 +0530

    Recursion revise

--- recursion/factorial.cpp
+++ recursion/factorial.cpp
@@ -12,23 +12,21 @@ using namespace std;
 }
 */
 
-int sum(int n)//functional recursion
+int factorial(int n)//functional recursion
 {
-    if(n==1){
+    if(n==0){
         return 1;
 
     }
-    if(n==0){
-        return 0;
-    }
-    return n+sum(n-1);
+
+    return n*factorial(n-1);
 }
 int main()
 {
     int n;
     cin >> n;
 
-   int result = sum(n);
+   int result = factorial(n);
     cout << result << endl;
     return 0;
 }
