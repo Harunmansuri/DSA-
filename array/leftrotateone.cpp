502104aebab85b224ba5aa9f9fba44b09944ab91
Harun mansuri <mrharun961766@gmail.com>
2026-01-26 10:59:40 +0530

    Revisoning array

---
+++ array/leftrotateone.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void leftrotateone(vector<int> &arr, int n)
+{
+    int temp = arr[0];
+    for (int i = 1; i < n; i++)
+    {
+        arr[i - 1] = arr[i];
+    }
+    arr[n - 1] = temp;
+}
+
+// Right rotate array by k positions
+void drotate(vector<int> &arr, int n, int k)
+{
+
+
+    k = k % n;
+    // Step 1: store first k elements
+    vector<int> temp;
+    for (int i = 0; i < k; i++)
+    {
+        temp.push_back(arr[i]);
+    }
+    // Step 2: shift remaining elements to left
+    for (int i = k; i < n; i++)
+    {
+        arr[i - k] = arr[i];
+    }
+    // Step 3: put stored elements at the end
+    // for (int i = 0; i < k; i++) {
+    //     arr[n - k + i] = temp[i];
+    // }
+    for (int i = n - k; i < n; i++)
+    {
+        arr[i] = temp[i - (n - k)];
+    }
+}
+
+void printarray(vector<int> &arr, int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+int main()
+{
+    int n;
+    cin >> n;
+    vector<int> arr(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    int k;
+    cin >> k;
+
+    // leftrotateone(arr, n);
+    drotate(arr, n, k);
+    printarray(arr, n);
+}
